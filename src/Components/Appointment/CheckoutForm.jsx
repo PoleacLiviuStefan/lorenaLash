6d6d24fa6c4b8c6955b577b111d69e6038714b6e
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2023-10-27 22:11:48 +1200

    finalstripoe

--- src/Components/Appointment/CheckoutForm.jsx
+++ src/Components/Appointment/CheckoutForm.jsx
@@ -6,13 +6,17 @@ import {
 } from '@stripe/react-stripe-js';
 
 const CheckoutForm = () => {
-    
+  const SERVER_IP = "http://localhost:5005";
     const [clientSecret, setClientSecret] = useState('');
     const stripePromise= loadStripe("pk_live_51MroBpCV1XqGrlRbJMZ8BZ6cFMqZjpa5yCxEknMWc2ioPxrO2V9VhGZm77CMOtYF1vo6hzw85kbC64bJwvIkg2OG00SxxOnm59")
     useEffect(() => {
       // Create a Checkout Session as soon as the page loads
-      fetch("/checkout-session", {
+      fetch(SERVER_IP + '/api/checkout-session', {
         method: "POST",
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
       })
         .then((res) => res.json())
         .then((data) => setClientSecret(data.clientSecret));
